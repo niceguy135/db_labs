5e990ab4ee89353024ee0679f16d83f7b75d64b9
Данов Константин <boss.danov1337@bebra.ru>
2023-11-11 18:31:46 +0300

    точно доделал лабу 7

--- Lab7/task5.sql
+++ Lab7/task5.sql
@@ -1,15 +1,28 @@
--- CREATE OR REPLACE FUNCTION control_student_age()
---   RETURNS TRIGGER 
---   LANGUAGE PLPGSQL
---   AS
--- $$
--- BEGIN
---     IF datediff(yy, NEW.birthday, getdate()) >= 100 THEN
---         RAISE NOTICE 'Студент слишком старый для всего этого дерьма...';
---     END IF;
---     RETURN NEW;
--- END;
--- $$
+CREATE OR REPLACE FUNCTION add_to_debtor() RETURNS trigger AS $emp_stamp$
+    declare
+        student_rec record;
+    BEGIN
+        IF NEW.mark = 2 THEN
+            SELECT surname, name, patronymic, students_group_number
+            into student_rec
+            from student
+            where student_id = NEW.student_id;
+
+            INSERT INTO debtor_students (surname, name, patronymic, group_id, debt_subject_id)
+            VALUES
+            (student_rec.surname, student_rec.name, student_rec.patronymic, student_rec.students_group_number, NEW.field);
+        END IF;
+        RETURN NEW;
+    END;
+$emp_stamp$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER add_to_debtor_trigger
+  AFTER UPDATE OF mark
+  ON field_comprehension
+  FOR ROW
+  EXECUTE FUNCTION add_to_debtor();
+
+------------------------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION control_student_age() RETURNS trigger AS $emp_stamp$
     BEGIN
@@ -19,7 +32,6 @@ CREATE OR REPLACE FUNCTION control_student_age() RETURNS trigger AS $emp_stamp$
         RETURN NEW;
     END;
 $emp_stamp$ LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE TRIGGER control_student_age_trigger
   BEFORE INSERT
